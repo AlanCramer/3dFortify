f8f67ea82839861d92bf610741f05cff949fd45e
alan.cramer <alan.cramer@hmhco.com>
2017-10-28 08:28:16 -0400

    added stl to scene

--- app/renderer.js
+++ app/renderer.js
@@ -16,27 +16,29 @@ var scene, camera, renderer;
 var geometry, material, mesh;
 
 init();
-animate();
+
 
 function init() {
 
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    camera.position.z = 100;
 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    //geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    //material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    //mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
 
     var loader = new STLLoader()
 
     loader.load('../stl/Body1.stl', function (geometry) {
       var material = new THREE.MeshNormalMaterial()
-      var mesh = new THREE.Mesh(geometry, material)
+      mesh = new THREE.Mesh(geometry, material)
       scene.add(mesh)
+
+      animate();
     })
 
 
